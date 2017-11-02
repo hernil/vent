6d7e80183765f200deb29ba00e4c2a15d3b9e8dc
Nils Herde <mail@hernil.com>
2017-11-02 22:49:14 +0100

    Frontend fiddling. Initial highcharts POC

--- vent-frontend/src/components/HelloWorld.vue
+++ vent-frontend/src/components/HelloWorld.vue
@@ -1,34 +1,36 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h1>{{ msg.content }}</h1>
+    <DataView/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data() {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-    };
-  },
-};
+  import axios from 'axios';
+  import DataView from './Data';
+
+  export default {
+    name: 'HelloWorld',
+    components: {
+      DataView,
+    },
+    data() {
+      return {
+        msg: { content: 'Hei verden!' },
+        errors: [],
+      };
+    },
+    created() {
+      axios.get('http://localhost:8080/greeting?name=Nils')
+        .then((response) => {
+          // JSON responses are automatically parsed.
+          this.msg = response.data;
+        })
+        .catch((e) => {
+          this.errors.push(e);
+        });
+    },
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->

e34a7ebcf3df9ba65dfe47a4530b77dc0b9fc9d2
Nils Herde <mail@hernil.com>
2017-11-28 16:41:51 +0100

    WIP Multiple datasources Two working connections. Will test more

--- vent-backend/src/main/kotlin/com/hernil/vent/VentApplication.kt
+++ vent-backend/src/main/kotlin/com/hernil/vent/VentApplication.kt
@@ -1,11 +1,16 @@
 package com.hernil.vent
 
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 
 @SpringBootApplication
-class VentApplication
-
-fun main(args: Array<String>) {
-    SpringApplication.run(VentApplication::class.java, *args)
+@EnableAutoConfiguration(exclude = arrayOf(DataSourceAutoConfiguration::class))
+class VentApplication {
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            SpringApplication.run(VentApplication::class.java, *args)
+        }
+    }
 }

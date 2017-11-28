5d14ca186ea21580366838a8af292470a28ca631
Nils Herde <mail@hernil.com>
2017-11-28 16:41:51 +0100

    WIP Multiple datasources Two working connections. Will test more

--- vent-backend/src/main/kotlin/com/hernil/vent/controllers/StudentController.kt
+++ vent-backend/src/main/kotlin/com/hernil/vent/controllers/StudentController.kt
@@ -1,12 +1,13 @@
 package com.hernil.vent.controllers
 
 import com.hernil.vent.domain.StudentRepository
+import com.hernil.vent.protus.LearnersRepository
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @CrossOrigin(origins = arrayOf("http://localhost:3000"))
 @RequestMapping("/student")
-class StudentController(val repository: StudentRepository) {
+class StudentController(val repository: LearnersRepository) {
 
     @GetMapping(value = "/all")
     fun getData() = repository.findAll()

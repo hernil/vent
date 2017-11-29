9843e305cb5d2594065aae6a3f9c9e79430ecc8b
Nils Herde <mail@hernil.com>
2017-11-29 10:34:52 +0100

    Some minor cleanup

--- vent-backend/src/main/kotlin/com/hernil/vent/application/domain/Student.kt
+++ vent-backend/src/main/kotlin/com/hernil/vent/application/domain/Student.kt
@@ -2,7 +2,6 @@ package com.hernil.vent.application.domain
 
 import com.hernil.vent.application.domain.mappers.CourseStudents
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 import javax.persistence.*
 
 @Entity
@@ -15,5 +14,4 @@ data class Student(@Id
 ) : Person(name) {
 }
 
-@Repository
 interface StudentRepository : JpaRepository<Student, Long>

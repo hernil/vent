9226b207295478ae3cf8eb9e810f28625ee631cd
Nils Herde <mail@hernil.com>
2018-01-05 13:39:56 +0100

    Running stable 1.5.9 Still need to get compiling and running jar

--- vent-backend/src/main/kotlin/com/hernil/vent/VentConfig.java
+++ vent-backend/src/main/kotlin/com/hernil/vent/VentConfig.java
@@ -4,9 +4,9 @@ import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -40,7 +40,7 @@ public class VentConfig {
                 .dataSource(dataSource)
                 .packages("com.hernil.vent.application")
                 .persistenceUnit("vent")
-                .properties(jpaProperties.getHibernateProperties("dataSource"))
+                .properties(jpaProperties.getHibernateProperties(dataSource))
                 .build();
     }
 

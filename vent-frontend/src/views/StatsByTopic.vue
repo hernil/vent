e19dab273a54da02d9b1352dd1bcc88ab7a8ab41
Nils Herde <mail@hernil.com>
2018-05-26 18:27:48 +0200

    Add some new views

---
+++ vent-frontend/src/views/StatsByTopic.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h2>Content usage by topic:</h2>
+    <c-usage-topic :units=units></c-usage-topic>
+    <h2>Content completion by topic:</h2>
+    <c-completion-topic :units=units></c-completion-topic>
+    <h2>Content recommended-rate by topic:</h2>
+    <c-recommended-rate-topic :units=units></c-recommended-rate-topic>
+  </div>
+</template>
+
+<script>
+
+  import ContentUsageByTopic from './subviews/ContentUsageByTopic';
+  import ContentCompletionByTopic from './subviews/ContentCompletionByTopic';
+  import ContentRecommendedRateByTopic from './subviews/ContentRecommendedRateByTopic';
+
+  import { store } from '../main';
+
+  export default {
+    components: {
+      CUsageTopic: ContentUsageByTopic,
+      CCompletionTopic: ContentCompletionByTopic,
+      CRecommendedRateTopic: ContentRecommendedRateByTopic,
+    },
+    data() {
+      return {
+        store,
+      };
+    },
+    computed: {
+      units() {
+        return this.store.course.units;
+      },
+    },
+  };
+
+</script>
+
+<style scoped>
+  pre {
+
+  }
+</style>
---
+++ vent-frontend/src/views/StatsByType.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h2>Content usage by type:</h2>
+    <c-usage-type :units=units></c-usage-type>
+    <h2>Content completion by type:</h2>
+    <c-completion-type :units=units></c-completion-type>
+    <h2>Content recommended-rate by type:</h2>
+    <c-recommended-rate-type :units=units></c-recommended-rate-type>
+  </div>
+</template>
+
+<script>
+
+  import ContentUsageByType from './subviews/ContentUsageByType';
+  import ContentCompletionByType from './subviews/ContentCompletionByType';
+  import ContentRecommendedRateByType from './subviews/ContentRecommendedRateByType';
+
+  import { store } from '../main';
+
+  export default {
+    components: {
+      CUsageType: ContentUsageByType,
+      CCompletionType: ContentCompletionByType,
+      CRecommendedRateType: ContentRecommendedRateByType,
+    },
+    data() {
+      return {
+        store,
+      };
+    },
+    computed: {
+      units() {
+        return this.store.course.units;
+      },
+    },
+  };
+
+</script>
+
+<style scoped>
+  pre {
+
+  }
+</style>
